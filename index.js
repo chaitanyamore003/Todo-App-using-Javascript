9479992f6b63a4baa419616a33395825a363d298
chaitanyamore003 <chaitanyamore003@gmail.com>
2025-11-09 12:39:49 +0530

    Added localStorage integration to To-Do app

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-let todoList = [
-    {
-        item:'buy Milk' ,
-        dueDate:'16/09/2025'
-    },
-    {
-        item:'dance' ,
-        dueDate:'16/09/2025'
-    } ];
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
 displayItems();
 
-function addTodo(){
-    let inputElement = document.querySelector('#input');
-    let dateElement = document.querySelector('#date');
-    let todoitem = inputElement.value;
-    let tododate = dateElement.value;
-    todoList.push({item: todoitem, dueDate: tododate});
-    inputElement.value = '';
-    dateElement.value = '';
-    displayItems();
+function addTodo() {
+  let inputElement = document.querySelector("#input");
+  let dateElement = document.querySelector("#date");
+  let todoitem = inputElement.value.trim();
+  let tododate = dateElement.value;
+
+  if (todoitem === "") {
+    alert("Please enter the todo task");
+    return;
+  } else if (tododate === "") {
+    alert("Please enter the date");
+    return;
+  }
+
+  todoList.push({ item: todoitem, dueDate: tododate });
+  inputElement.value = "";
+  dateElement.value = "";
+
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  displayItems();
 }
 
-function displayItems(){
-    let containerElement = document.querySelector('.todo-container');
-    let newhtml = '';
-    for(let i = 0; i < todoList.length; i++){
-        let {item, dueDate} = todoList[i];
-        newhtml += `
-            <span>${item}</span>
-            <span>${dueDate}</span>
-            <button onclick="todoList.splice(${i},1); displayItems()" class = "delete-btn">Delete</button>
-            `;
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector(".todo-container");
+  let newhtml = "";
+
+  if (todoList.length <= 0) {
+    newhtml = `<h2>Add Todo To Display</h2>`;
+  } else {
+    for (let i = 0; i < todoList.length; i++) {
+      let { item, dueDate } = todoList[i];
+      newhtml += `
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button onclick="deleteTodo(${i})" class="delete-btn">Delete</button>
+      `;
     }
-    containerElement.innerHTML = newhtml;
+  }
+
+  containerElement.innerHTML = newhtml;
 }
